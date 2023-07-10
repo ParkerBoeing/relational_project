f151fc4a65a01d541d862d66623b6e6a8351dd42
Parker Boeing <parker.boeing@gmail.com>
2023-07-09 22:21:05 -0600

    Feat: user story 14

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -6,4 +6,19 @@ class RoutesController < ApplicationController
   def show
     @route = Route.find(params[:id])
   end
+
+  def edit
+    @route = Route.find(params[:id])
+  end
+
+  def update
+    route = Route.find(params[:id])
+    route.update({
+      name: params[:route][:name],
+      meters_tall: params[:route][:meters_tall],
+      bolted: params[:route][:bolted]
+    })
+    route.save
+    redirect_to "/routes/#{route.id}"
+  end
 end
