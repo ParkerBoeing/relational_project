c171d8b1281997f7aa794cabfd471e704db3cdf7
Parker Boeing <parker.boeing@gmail.com>
2023-07-09 22:42:42 -0600

    Test: User story 15

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,7 @@
 class Route < ApplicationRecord
   belongs_to :crag
+
+  def self.bolted_routes
+    where(bolted:true)
+  end
 end
--- spec/features/routes/show_spec.rb
+++ spec/features/routes/show_spec.rb
@@ -2,21 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'routes page' do
   before :each do
-    @crag_1 = Crag.create!(name: "Watchtower", elevation: 4500, nearby_camping: false)
+    @crag_1 = Crag.create!(name: "Watchtower", elevation: 4500, nearby_camping: false, created_at: 1.day.ago)
     @crag_2 = Crag.create!(name: "Diablo", elevation: 9000, nearby_camping: true)
-    @route_1 = @crag_1.routes.create!(name: "Malvado", meters_tall: 15, bolted: true)
-    @route_2 = @crag_1.routes.create!(name: "Inferno", meters_tall: 10, bolted: true)
+    @route_1 = @crag_1.routes.create!(name: "Extreme Unction", meters_tall: 20, bolted: false)
+    @route_2 = @crag_1.routes.create!(name: "Invocation", meters_tall: 25, bolted: true)
+    @route_3 = @crag_2.routes.create!(name: "Malvado", meters_tall: 15, bolted: true)
+    @route_4 = @crag_2.routes.create!(name: "Inferno", meters_tall: 10, bolted: true)
   end
   describe 'When I visit /routes' do
     it 'shows each route in the table as well as the routes attributes' do
       visit "/routes"
-      expect(page).to have_content(@route_1.name)
-      expect(page).to have_content(@route_1.id)
-      expect(page).to have_content(@route_1.meters_tall)
-      expect(page).to have_content(@route_1.bolted)
-      expect(page).to have_content(@route_1.created_at)
-      expect(page).to have_content(@route_1.updated_at)
-      expect(page).to have_content(@route_1.crag_id)
+      # expect(page).to_not have_content(@route_1.name)
+      # expect(page).to_not have_content(@route_1.bolted)
+
+
       expect(page).to have_content(@route_2.name)
       expect(page).to have_content(@route_2.id)
       expect(page).to have_content(@route_2.meters_tall)
@@ -24,6 +23,22 @@ RSpec.describe 'routes page' do
       expect(page).to have_content(@route_2.created_at)
       expect(page).to have_content(@route_2.updated_at)
       expect(page).to have_content(@route_2.crag_id)
+
+      expect(page).to have_content(@route_3.name)
+      expect(page).to have_content(@route_3.id)
+      expect(page).to have_content(@route_3.meters_tall)
+      expect(page).to have_content(@route_3.bolted)
+      expect(page).to have_content(@route_3.created_at)
+      expect(page).to have_content(@route_3.updated_at)
+      expect(page).to have_content(@route_3.crag_id)
+
+      expect(page).to have_content(@route_4.name)
+      expect(page).to have_content(@route_4.id)
+      expect(page).to have_content(@route_4.meters_tall)
+      expect(page).to have_content(@route_4.bolted)
+      expect(page).to have_content(@route_4.created_at)
+      expect(page).to have_content(@route_4.updated_at)
+      expect(page).to have_content(@route_4.crag_id)
     end
 
     it 'has a link back to the crags index page' do
