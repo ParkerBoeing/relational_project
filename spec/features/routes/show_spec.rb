b5bb2087c6cbdd537754855d19274b6e90091c67
Parker Boeing <parker.boeing@gmail.com>
2023-07-09 22:14:13 -0600

    Test: User story 14

--- spec/features/routes/show_spec.rb
+++ spec/features/routes/show_spec.rb
@@ -79,15 +79,4 @@ RSpec.describe 'routes page' do
       expect(page).to_not have_content(true)
     end
   end
-    # User Story 14, Child Update 
-
-    # As a visitor
-    # When I visit a Child Show page
-    # Then I see a link to update that Child "Update Child"
-    # When I click the link
-    # I am taken to '/child_table_name/:id/edit' where I see a form to edit the child's attributes:
-    # When I click the button to submit the form "Update Child"
-    # Then a `PATCH` request is sent to '/child_table_name/:id',
-    # the child's data is updated,
-    # and I am redirected to the Child Show page where I see the Child's updated information
 end
